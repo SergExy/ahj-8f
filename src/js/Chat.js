0ce6c0f6dcaf6b54995ade0a76ef9f27334c9c74
Seryozha <syt.an@ya.ru>
2024-01-20 19:47:16 +0300

    fix error

--- src/js/Chat.js
+++ src/js/Chat.js
@@ -156,6 +156,9 @@ export default class Chat {
       };
       const registry = await this.api.registry(JSON.stringify(data));
       if (registry.status === 'error') {
+        const hint = formGroupEl.querySelector('.form__hint');
+        if (!hint) return;
+
         const hintEl = createElement({
           name: 'div',
           classes: ['form__hint'],
